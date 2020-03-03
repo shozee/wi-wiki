fc87b60c6a5c93e76b2b6a974dddaf60bb119353
Shoji Hirahara <s_hirahara@met.kishou.go.jp>
2020-03-04 07:57:01 +0900

    support: remove warning in wiki.c

--- wrapper/wiki.c
+++ wrapper/wiki.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <errno.h>
+#include <unistd.h>
 #include <sys/wait.h>
 
 extern char **environ;
@@ -13,28 +14,24 @@ int main(int argc, char *argv[]) {
     int     rc = 1;
     char  command[256];
 
-    /* 実行スクリプト設定 */
-    strcpy(command, "wi.sh");        // 対象スクリプトの絶対パスを設定
+    strcpy(command, "wi.sh");
 
     pid = fork();
 
     switch(pid) {
     case 0:
-        /* 子プロセス */
-        /* スクリプト実行 */
-        setgid( GID );        // setuidを設定したいuserのgid
-        setuid( UID );        // setuidを設定したいuserのuid
+        /* child process */
+        setgid( GID );
+        setuid( UID );
         if (execve(command, argv, environ) < 0) {
             exit(EXIT_FAILURE);
         }
         break;
     case -1:
-        /* 失敗 */
         exit(EXIT_FAILURE);
         break;
     default:
-        /* 親プロセス */
-        /* 子プロセスの完了を待つ */
+        /* parent process */
         waitpid(pid, &status, 0);
         if (WIFEXITED(status)) {
             rc = WEXITSTATUS(status);
